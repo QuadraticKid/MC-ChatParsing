7d2cd334f6b48f6895083614222a6d5b1f7ce279
Quad <gcgithub@bar.edu>
2020-07-17 21:45:35 -0700

    Quick patch with colors for translations

--- translateBot.js
+++ translateBot.js
@@ -22,117 +22,114 @@ bot.on("chat", (msgData, packetInfo) => {
 });
 
 function processMsg(data) {
-    
-
-  if (
-    typeof data.text == "undefined" &&
-    typeof data.translate !== "undefined" && 
-    !(typeof data == 'string')
-  ) {
-    var msg = "";
-
-    var translate = translations[data.translate] || data.translate;
-
-    // console.log(translate);
-
-    var withdata = [];
-
-    if (typeof data.with !== "undefined")
-      data.with.forEach(data => {
-        //   console.log(data)
-        withdata.push(processMsg(data));
-      });
-
-    // withdata.forEach(element => {
-        // console.log(element)
-    // });
-    // console.log(withdata)
-
+  console.log(data)
+    if (
+      typeof data.text == "undefined" &&
+      typeof data.translate !== "undefined" && 
+      !(typeof data == 'string')
+    ) {
+      var msg = "";
+  
+      var translate = translations[data.translate] || data.translate;
+  
+      // console.log(translate);
+  
+      var withdata = [];
+  
+      if (typeof data.with !== "undefined")
+        data.with.forEach(data => {
+          //   console.log(data)
+          withdata.push(processMsg(data));
+        });
+  
+  
    
+  
+  
+      if (typeof data.text == "undefined") data.text = "";
+      if (typeof data.color == "undefined") data.color = "";
+      if (typeof data.bold == "undefined") data.bold = false;
+      if (typeof data.obfuscated == "undefined") data.obfuscated = false;
+      if (typeof data.underlined == "undefined") data.underlined = false;
+      if (typeof data.italic == "undefined") data.italic = false;
+      if (typeof data.strikethrough == "undefined") data.strikethrough = false;
+  
+      var color = getColor(data.color);
+      var bold = data.bold == true ? "§l" : "";
+      var obfuscated = data.obfuscated == true ? "§k" : "";
+      var underlined = data.underlined == true ? "§n" : "";
+      var italic = data.italic == true ? "§o" : "";
+      var strikethrough = data.strikethrough == true ? "§m" : "";
+  
+      //strikethrough
+      // /obfuscated
 
-    // msg = translate
-
-
-    for(var i = 0;i<(translate.match(/%s/g) || []).length+1;i++){
+      for(var i = 0;i<(translate.match(/%s/g) || []).length+1;i++){
         var replace = typeof withdata[i] !== 'undefined'?withdata[i]:"";
-        translate = translate.replace('%s',replace)    
+        translate = translate.replace('%s',replace+color +
+        bold +
+        obfuscated +
+        underlined +
+        italic +
+        strikethrough)    
     }
 
-
-
-    if (typeof data.text == "undefined") data.text = "";
-    if (typeof data.color == "undefined") data.color = "";
-    if (typeof data.bold == "undefined") data.bold = false;
-    if (typeof data.obfuscated == "undefined") data.obfuscated = false;
-    if (typeof data.underlined == "undefined") data.underlined = false;
-    if (typeof data.italic == "undefined") data.italic = false;
-    if (typeof data.strikethrough == "undefined") data.strikethrough = false;
-
-    var color = getColor(data.color);
-    var bold = data.bold == true ? "§l" : "";
-    var obfuscated = data.obfuscated == true ? "§k" : "";
-    var underlined = data.underlined == true ? "§n" : "";
-    var italic = data.italic == true ? "§o" : "";
-    var strikethrough = data.strikethrough == true ? "§m" : "";
-
-    //strikethrough
-    // /obfuscated
-
-    var msg =
-      color +
-      bold +
-      obfuscated +
-      underlined +
-      italic +
-      strikethrough + translate
-
-     if (typeof data.extra !== "undefined")
-      data.extra.forEach(data => {
-        msg = msg + processMsg(data);
-      });
-    //   console.log(msg)
+  
+      var msg =
+        color +
+        bold +
+        obfuscated +
+        underlined +
+        italic +
+        strikethrough + translate
+  
+       if (typeof data.extra !== "undefined")
+        data.extra.forEach(data => {
+          msg = msg + processMsg(data);
+        });
+      //   console.log(msg)
+        return msg;
+  
+        
+    } else if (typeof data == 'string') {
+  
+        return data;
+    } else {
+      if (typeof data.text == "undefined") data.text = "";
+      if (typeof data.color == "undefined") data.color = "";
+      if (typeof data.bold == "undefined") data.bold = false;
+      if (typeof data.obfuscated == "undefined") data.obfuscated = false;
+      if (typeof data.underlined == "undefined") data.underlined = false;
+      if (typeof data.italic == "undefined") data.italic = false;
+      if (typeof data.strikethrough == "undefined") data.strikethrough = false;
+  
+      var color = getColor(data.color);
+      var bold = data.bold == true ? "§l" : "";
+      var obfuscated = data.obfuscated == true ? "§k" : "";
+      var underlined = data.underlined == true ? "§n" : "";
+      var italic = data.italic == true ? "§o" : "";
+      var strikethrough = data.strikethrough == true ? "§m" : "";
+  
+      //strikethrough
+      // /obfuscated
+  
+      var msg =
+        color +
+        bold +
+        obfuscated +
+        underlined +
+        italic +
+        strikethrough +
+        data.text;
+  
+      if (typeof data.extra !== "undefined")
+        data.extra.forEach(data => {
+          msg = msg + processMsg(data);
+        });
+  
       return msg;
-
-      
-  } else if (typeof data == 'string') {
-
-      return data;
-  } else {
-    if (typeof data.text == "undefined") data.text = "";
-    if (typeof data.color == "undefined") data.color = "";
-    if (typeof data.bold == "undefined") data.bold = false;
-    if (typeof data.obfuscated == "undefined") data.obfuscated = false;
-    if (typeof data.underlined == "undefined") data.underlined = false;
-    if (typeof data.italic == "undefined") data.italic = false;
-    if (typeof data.strikethrough == "undefined") data.strikethrough = false;
-
-    var color = getColor(data.color);
-    var bold = data.bold == true ? "§l" : "";
-    var obfuscated = data.obfuscated == true ? "§k" : "";
-    var underlined = data.underlined == true ? "§n" : "";
-    var italic = data.italic == true ? "§o" : "";
-    var strikethrough = data.strikethrough == true ? "§m" : "";
-
-    //strikethrough
-    // /obfuscated
-
-    var msg =
-      color +
-      bold +
-      obfuscated +
-      underlined +
-      italic +
-      strikethrough +
-      data.text;
-
-    if (typeof data.extra !== "undefined")
-      data.extra.forEach(data => {
-        msg = msg + processMsg(data);
-      });
-
-    return msg;
+    }
   }
-}
 
 function getColor(color) {
   if (color == "reset") {
