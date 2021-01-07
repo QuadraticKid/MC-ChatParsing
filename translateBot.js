3dfc14fd2c2e2011130aef0db30a3f3621d9033e
Apmunute <munuteaugusto@gmail.com>
2021-01-07 14:39:39 -0300

    fix reset color issue

--- translateBot.js
+++ translateBot.js
@@ -42,7 +42,7 @@ function processMsg(data) {
   
   
       if (typeof data.text == "undefined") data.text = "";
-      if (typeof data.color == "undefined") data.color = "";
+      if (typeof data.color == "undefined") data.color = "reset";
       if (typeof data.bold == "undefined") data.bold = false;
       if (typeof data.obfuscated == "undefined") data.obfuscated = false;
       if (typeof data.underlined == "undefined") data.underlined = false;
@@ -89,7 +89,7 @@ function processMsg(data) {
         return data;
     } else {
       if (typeof data.text == "undefined") data.text = "";
-      if (typeof data.color == "undefined") data.color = "";
+      if (typeof data.color == "undefined") data.color = "reset";
       if (typeof data.bold == "undefined") data.bold = false;
       if (typeof data.obfuscated == "undefined") data.obfuscated = false;
       if (typeof data.underlined == "undefined") data.underlined = false;
